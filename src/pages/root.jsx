f0db68fc8e5e78b8a158d3637479a4a9a8387453
EstebanBravo1 <es.bravoa@duocuc.cl>
2025-10-22 18:32:41 -0300

    Creación página web + Cración de componente Footer

--- src/pages/root.jsx
+++ src/pages/root.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Outlet, useNavigation } from "react-router"
 import Header from "../components/Header"
+import Footer from "../components/Footer"
 
 export default function Root() {
   const navigation = useNavigation()
@@ -16,6 +17,7 @@ export default function Root() {
         ) : (
           <Outlet />
         )}
+        <Footer />
       </main>
     </>
   )
