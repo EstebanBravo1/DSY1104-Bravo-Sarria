cc629c3494e03c28ccf1c1510d992453c89b8adc
alvaro <al.sarria@duocuc.cl>
2025-10-23 10:46:58 -0300

    feat(HH-003): Implementar header completo con navegación

---
+++ src/components/Header.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { Navbar, Nav, Container, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import logo from '/assets/LogoHuertoHogar.png';
+import './Header.css';
+
+export default function Header() {
+
+  const [query, setQuery] = useState('');
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log('Buscando:', query);
+  };
+
+  return (
+    <header>
+      <Navbar bg="light" expand="md" fixed="top" className="custom-navbar">
+        <Container fluid>
+          <Navbar.Brand href="/"><img src={logo} alt="Huerto Hogar Logo" className="logo"/></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <LinkContainer to="/productos">
+                <Nav.Link>Productos</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/contacto">
+                <Nav.Link>Contacto</Nav.Link>
+              </LinkContainer>
+                 <LinkContainer to="/carrito">
+                <Nav.Link>🛒 Carrito</Nav.Link>
+              </LinkContainer>
+            </Nav>
+            <Nav className="search-container">
+              <Form className="d-flex" onSubmit={handleSearch}>
+                <FormControl
+                  type="search"
+                  placeholder="Buscar productos..."
+                  className="search-input"
+                  value={query}
+                  onChange={(e) => setQuery(e.target.value)}
+                />
+                <Button variant="outline-success" type="submit" className="search-btn">
+                  Buscar
+                </Button>
+              </Form>
+            </Nav>
+            <Nav>
+              <LinkContainer to="/login">
+                <Nav.Link className="login-btn">Iniciar Sesión</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/register">
+                <Nav.Link className="register-btn">Registrarse</Nav.Link>
+              </LinkContainer>
+           
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
