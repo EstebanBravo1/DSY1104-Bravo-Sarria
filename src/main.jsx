d911ae18c5471bba50ae3cd203f2478318d76fc4
EstebanBravo1 <es.bravoa@duocuc.cl>
2025-10-26 00:52:26 -0300

    Corección main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Index.css'
+import './index.css'
 import { router } from './routes'
 import { AuthProvider } from './context/AuthContext'
 
